ace34e4a5815a97436f3a1fd9aa5886dd5cb8133
Kais Chebata <qays.sh@gmail.com>
2020-11-04 00:38:11 +0100

    adding caption and icon to game window

--- SpaceInvaders/main.py
+++ SpaceInvaders/main.py
@@ -1,14 +1,19 @@
 import pygame
-# import pygame
 
-# initializing pygame
+# intialize pygame
 pygame.init()
 
-# creating screen
+# set game screen
 screen = pygame.display.set_mode((800, 600))
 
-# waiting until exit
-running = True 
+# Title and Icon
+pygame.display.set_caption('Space Invaders')
+icon = pygame.image.load('SpaceInvaders/raw_data/space_invaders_icon.png')
+pygame.display.set_icon(icon)
+
+# Game loop
+running = True
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
