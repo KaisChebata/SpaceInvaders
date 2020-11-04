5b2e6045be5667a7716672f844de15f3b9672e32
Kais Chebata <qays.sh@gmail.com>
2020-11-04 20:40:07 +0100

    adding enemy and set its movements

--- SpaceInvaders/main.py
+++ SpaceInvaders/main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 # intialize pygame
 pygame.init()
@@ -11,13 +12,22 @@ pygame.display.set_caption('Space Invaders')
 icon = pygame.image.load('SpaceInvaders/raw_data/space_invaders_icon.png')
 pygame.display.set_icon(icon)
 
+# Enemy
+enemy_image = pygame.image.load('SpaceInvaders/raw_data/enemy.png')
+enemy_x = random.randint(0, 736)
+enemy_y = random.randint(50, 150)
+enemy_xshift, enemy_yshift = 0.3, 40
+
 # Player
 player_img = pygame.image.load('SpaceInvaders/raw_data/player.png')
 player_x, player_y = 370, 480
-x_shift, y_shift = 0, 0
+player_xshift = 0
 
 def player(x_axis, y_axis):
     screen.blit(player_img, (x_axis, y_axis))
+
+def enemy(x_axis, y_axis):
+    screen.blit(enemy_image, (x_axis, y_axis))
 
 # Game loop
 running = True
@@ -33,27 +43,41 @@ while running:
     
         # Checking Keystrokes directions
         if event.type == pygame.KEYDOWN:
-            # print('a keystroke is pressed ....')
+            # print('a keystroke is pressed ....') # this comment and other similar to for debugging
             if event.key == pygame.K_LEFT:
                 # print('left arrow is pressed ....')
-                x_shift = -0.3
+                player_xshift = -0.3
             if event.key == pygame.K_RIGHT:
                 # print('right arrow is pressed ....')
-                x_shift = 0.3
+                player_xshift = 0.3
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 # print('keystroke has been released ....')
-                x_shift = 0
+                player_xshift = 0
 
-    # changing player X axis before drawing it.
-    player_x += x_shift
+    # Checking X axis Boundaries for Spaceship before drawing it.
+    player_x += player_xshift
+    
     if player_x <= 0:
         player_x = 0
     elif player_x >= 736:
-        player_x =736 
+        player_x = 736 
+    
+    # Enemy Movements
+    enemy_x += enemy_xshift
+
+    if enemy_x <= 0:
+        enemy_xshift = 0.3
+        enemy_y += enemy_yshift
+    elif enemy_x >= 736:
+        enemy_xshift = -0.3
+        enemy_y += enemy_yshift
     
     # drawing player
     player(player_x, player_y)
+
+    # drawing enemy
+    enemy(enemy_x, enemy_y)
 
     # updating screen
     pygame.display.update()
