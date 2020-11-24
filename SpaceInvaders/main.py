569315a77b2fc7504f20eee050977d197cb49677
Kais Chebata <qays.sh@gmail.com>
2020-11-24 17:33:17 +0100

    adding fire bullets function to spaceship

--- SpaceInvaders/main.py
+++ SpaceInvaders/main.py
@@ -21,6 +21,15 @@ enemy_x = random.randint(0, 736)
 enemy_y = random.randint(50, 150)
 enemy_xshift, enemy_yshift = 4.5, 40
 
+# Bullet
+# Bullet states:
+# ready: can't see bullet on screen
+# fire: the bullet is currently moving.
+bullet_img = pygame.image.load('SpaceInvaders/raw_data/bullet.png')
+bullet_x, bullet_y = 0, 480
+bullet_xshift, bullet_yshift = 0, 10
+bullet_state = 'ready'
+
 # Player
 player_img = pygame.image.load('SpaceInvaders/raw_data/player.png')
 player_x, player_y = 370, 480
@@ -32,6 +41,12 @@ def player(x_axis, y_axis):
 def enemy(x_axis, y_axis):
     screen.blit(enemy_image, (x_axis, y_axis))
 
+def fire_bullet(x_axis, y_axis):
+    global bullet_state
+    bullet_state = 'fire'
+
+    screen.blit(bullet_img, (x_axis + 16, y_axis + 10))
+
 # Game loop
 running = True
 
@@ -42,6 +57,7 @@ while running:
 
     # drawing Background Image
     screen.blit(background, (0, 0))
+
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -56,6 +72,12 @@ while running:
             if event.key == pygame.K_RIGHT:
                 # print('right arrow is pressed ....')
                 player_xshift = 5
+            if event.key == pygame.K_SPACE:
+                # stoping bullet from movement with spaceship after first shout
+                if bullet_state is 'ready':
+                    # get the currenct x cordinate of the spaceship
+                    bullet_x = player_x
+                    fire_bullet(bullet_x, bullet_y)
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 # print('keystroke has been released ....')
@@ -72,6 +94,7 @@ while running:
     # Enemy Movements
     enemy_x += enemy_xshift
 
+    # Checking X axis, Y axis Boundaries for Enemy
     if enemy_x <= 0:
         enemy_xshift = 4.5
         enemy_y += enemy_yshift
@@ -79,6 +102,15 @@ while running:
         enemy_xshift = -4.5
         enemy_y += enemy_yshift
     
+    # Bullet Movement
+    if bullet_y <= 0:
+        bullet_y = 485
+        bullet_state = 'ready'
+
+    if bullet_state is 'fire':
+        fire_bullet(bullet_x, bullet_y)
+        bullet_y -= bullet_yshift
+
     # drawing player
     player(player_x, player_y)
 
